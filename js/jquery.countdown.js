4bd174b53a8b2e5f40f90d0778c9fdca9760adfd
Evert-jvv <trevejvv@gmail.com>
2015-08-28 20:36:34 +0200

    Update jquery.countdown.js

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -111,7 +111,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       return this.init();
     };
     $.countdown.defaultOptions = {
-      date: "June 7, 2087 15:03:25",
+      date: "October 7, 2015 20:35:25",
       refresh: 1000,
       onEnd: $.noop,
       render: function(date) {
